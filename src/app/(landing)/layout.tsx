ff6bc131225a066c743c73ffc627a34d85d59980
Vagarth <vagarth419@gmail.com>
2024-09-18 23:10:00 +0530

    feat: create landing page

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import LandingPageNavbar from "./components/navbar"
+
+const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col container relative">
+      <LandingPageNavbar />
+      {children}
+    </div>
+  )
+}
+
+export default LandingPageLayout
