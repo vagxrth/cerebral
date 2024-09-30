2fef61349c0af0ae474a3534e9c370d7cc9652dc
Vagarth <vagarth419@gmail.com>
2024-09-30 22:06:57 +0530

    feat: create dashboard component

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,12 +1,12 @@
 import CallToAction from "./_components/call-to-action"
-// import DashboardSnippet from "./_components/dashboard-snippet"
+import Dashboard from "./_components/dashboard"
 
 export default function Home() {
   return (
     <main className="md:px-10 py-20 flex flex-col gap-36">
       <div>
         <CallToAction />
-        {/* <DashboardSnippet /> */}
+        <Dashboard />
       </div>
       {/* <PricingSection /> */}
     </main>
