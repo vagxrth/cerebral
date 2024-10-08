364c6fabe464ffcb636af09926268b53f77bcc06
Vagarth <vagarth419@gmail.com>
2024-10-08 22:52:37 +0530

    feat: modify signin and signup page

--- src/components/global/google-oauth/index.tsx
+++ src/components/global/google-oauth/index.tsx
@@ -2,8 +2,8 @@
 
 import { Button } from "@/components/ui/button"
 import { useGoogleAuth } from "@/hooks/authentication"
-import { Google } from "@/icons"
 import { Loader } from "lucide-react"
+import { useEffect, useState } from "react"
 
 type GoogleAuthButtonProps = {
   method: "signup" | "signin"
@@ -11,6 +11,29 @@ type GoogleAuthButtonProps = {
 
 export const GoogleAuthButton = ({ method }: GoogleAuthButtonProps) => {
   const { signUpWith, signInWith } = useGoogleAuth()
+  const [loading, setLoading] = useState(false)
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const handleClick = async () => {
+    setLoading(true)
+
+    try {
+      if (method === "signin") {
+        await signInWith("oauth_google")
+      } else {
+        await signUpWith("oauth_google")
+      }
+    } catch (error) {
+      console.error("Authentication failed", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Button
       {...(method === "signin"
@@ -22,11 +45,15 @@ export const GoogleAuthButton = ({ method }: GoogleAuthButtonProps) => {
           })}
       className="w-full rounded-2xl flex gap-3 bg-themeBlack border-themeGray"
       variant="outline"
+      disabled= {loading}
     >
-      <Loader loading={false}>
-        <Google />
-        Google
-      </Loader>
+      {loading ? (
+        <Loader className="animate-spin" />
+      ) : (
+        isClient && <img src="/google.svg" alt="Google" />
+      )}
+      Google
+
     </Button>
   )
 }
